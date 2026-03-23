94d992b21a4d270f8cdebd7a6ed1f19562d5e514
Reoverv <remco.o.2002@gmail.com>
2026-03-23 14:42:45 +0100

    cleanup

---
+++ Skribble/Line.cs
@@ -0,0 +1,38 @@
+﻿namespace Skribble;
+
+public class Line{
+    public Coords Start{ get; set; }
+    public Coords End{ get; set; }
+
+    public int Lenght{ get; set; }
+    public String Color{ get; set; }
+    public bool isSet{ get; set; }
+    public int lineNumber{ get; set; }
+
+    public Line(Coords start, bool isSet){
+        Start = start;
+        this.isSet = isSet;
+    }
+
+    public Line(Coords start, int lenght){
+        Start = start;
+        Lenght = lenght;
+    }
+
+
+}
+
+
+public class Coords{
+    public int X{ get; set; }
+    public int Y{ get; set; }
+
+    public Coords(int y, int x){
+        X = x;
+        Y = y;
+    }
+
+    public override string ToString(){
+        return $"{nameof(Y)}: {Y}, {nameof(X)}: {X}";
+    }
+}
---
+++ Skribble/colorPalette.cs
@@ -0,0 +1,21 @@
+﻿namespace Skribble;
+
+public class ColorPalette{
+
+    public String Name;
+    public int A;
+    public int R;
+    public int G;
+    public int B;
+
+    public ColorPalette(string name, int r, int g, int b){
+        Name = name;
+        A = 255;
+        R = r;
+        G = g;
+        B = b;
+    }
+
+
+}
+
